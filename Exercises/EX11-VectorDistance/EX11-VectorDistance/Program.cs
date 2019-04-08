0cb5088d7d355f0b248538f6a62f8f766db27c5e
Kindra Bilodeau <you@example.com>
2019-04-08 08:14:02 -0400

    Added Exercise 11-VectorDistance

---
+++ Exercises/EX11-VectorDistance/EX11-VectorDistance/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX11_VectorDistance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exercise 11: Vector Distance Calculation");
+            Cords[] cord = new Cords[100];
+            cord = Util.FillArray(cord);   //fill array with random numbers
+            Util.FindDistance(cord); //find distance between each point 
+            Cords[] cords3 = new Cords[1000];
+            
+            cords3 = Util.FillArray3Point(cords3);
+            Util.Distance3Point(cords3); 
+        }
+    }
+}
---
+++ Exercises/EX11-VectorDistance/EX11-VectorDistance/Util.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX11_VectorDistance
+{
+    class Util
+    {
+        //Fills the the array with random numbers 
+        public static Cords[] FillArray(Cords[] cord)
+        {
+            Random randy = new Random();
+            for (int i = 0; i < cord.Length; i++)
+            {
+                cord[i].x = randy.Next(1, 101);
+                cord[i].y = randy.Next(1, 101);
+            }
+            //This is here to check the array
+            //foreach (var c in cord)
+            //   Console.Write($"Cords({c.x},{c.y}), ");
+            return cord;
+        }
+
+        public static double FindDistance(Cords[] c)
+        {
+            double value;
+            double max = Int32.MaxValue;
+
+            for (int i = 0; i < c.Length; i++)
+                for (int j = i + 1; j < c.Length; j++)
+                {
+                    value = Math.Sqrt(Math.Pow(c[i].x - c[j].x, 2) + Math.Pow(c[i].y - c[j].y, 2));
+                    if (value < max)
+                    {
+                        max = value;
+                        Console.Write($"The closest points are at element {i},{j}: ");
+                        Console.Write($"({c[i].x},{c[i].y})({c[j].x},{c[j].y})");
+                        Console.WriteLine($"With A Distance:{value}");
+                        Console.WriteLine();
+                    }
+
+                }
+            return max;
+        }
+
+        public static Cords[] FillArray3Point(Cords[] cords)
+        {
+            Random randy = new Random();
+            for (int i = 0; i < cords.Length; i++)
+            {
+                cords[i].x = randy.Next(1, 1001);
+                cords[i].y = randy.Next(1, 1001);
+                cords[i].z = randy.Next(1, 1001);
+            }
+            //foreach (var c in cords)
+            //   Console.Write($"Cords({c.x},{c.y},{c.z}), ");
+            return cords;
+        }
+
+        public static void Distance3Point(Cords[] c)
+        {
+            double value;
+            double max = Int32.MaxValue;
+            for (int i = 0; i < c.Length; i++)
+                for (int j = i + 1; j < c.Length; j++)
+                {
+                    value = Math.Sqrt(Math.Pow(c[i].x - c[j].x, 2) + Math.Pow(c[i].y - c[j].y, 2) + Math.Pow(c[i].z - c[j].z, 2));
+                    if (value < max)
+                    {
+                        max = value;
+                        Console.Write($"The closest points are at element {i},{j}: ");
+                        Console.WriteLine($"({c[i].x},{c[i].y},{c[i].z})({c[j].x},{c[j].y},{c[j].z})");
+                        Console.WriteLine($"With A Distance of:{value}");
+                        Console.WriteLine();
+                    }
+                }
+        }
+    }
+}
